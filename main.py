6d3538fdfeb36cb7a6771a23ff5bf209eaabe7f0
Luis Pimenta <luisfelipearaujopimenta@gmail.com>
2023-12-31 18:14:09 -0300

    gitignore

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 import requests as req
 import pandas as pd
 import locale
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 locale.setlocale(locale.LC_MONETARY, 'pt_BR.UTF-8')
 
-nomes = pd.read_excel('teste.xlsx')
-pais = 'BRL'
-host = "https://economia.awesomeapi.com.br/"
+nomes = pd.read_excel(os.getenv('path_dados'))
+host = os.getenv('host')
+pais = os.getenv('pais')
 
 def busca_diaria():
     paises = []
@@ -18,11 +22,13 @@ def busca_diaria():
     my_string = ','.join(codes)
     api_url = host+"last/" + my_string
     request = req.get(api_url)
+    dados = request.json()
     if request.status_code == 200:
-        dados = request.json()
         print(f'Segue a ultima cotação das moedas pesquisadas: ')
-    for nome in paises:
-        print(f'{dados[nome]['name'] + ': ' + dados[nome]['high']}')
+        for nome in paises:
+            print(f'{dados[nome]['name'] + ': ' + dados[nome]['high']}')
 
+    else:
+        print(dados['message'])
 
 busca_diaria()
